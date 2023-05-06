53512b3bf25637b52aba7f3b79ced80de997dc20
powr <1410837981@qq.com>
2023-05-06 19:18:51 +0800

    feat: 实现布局

--- 02domParse/stateMachine.js
+++ 02domParse/stateMachine.js
@@ -4,15 +4,25 @@ const html = `<html lang="en">
 <head> 
     <title>Dom解析</title>
     <style>
-        html body .app{ width: 100px;}
+
+        #app{
+            width: 100px;
+        }
+        .app{
+            width: 200px;
+        }
+
         .num{
             width: 100px;
             display: block;
         }
+        .num {
+            height: 200px;
+        }
     </style>
 </head>
 <body>
-    <div id="app">文字文字
+    <div class="app" id="app">文字文字
     <span class="num">12121</span>
     </div>
  
@@ -23,27 +33,85 @@ let styleSheet = null
 function applyStyle(element) {
     if(!styleSheet) return 
     // console.log(element)
+    // console.log(styleSheet)
     let selectorStr = styleSheet.stylesheet.rules[0].selectors[0]
-    let selectorArr = selectorStr.split(' ')
-    selectorMathch(selectorArr[2], element)
-}
+    for(let rule of styleSheet.stylesheet.rules) {
+        selectorArr = rule.selectors[0].split(' ')
+        const specificity = selectorMatch(selectorArr[selectorArr.length -1], element)
+        if(specificity) {
+            // console.log(JSON.stringify(styleSheet, null, ' '))
+            const declarations = rule.declarations
+            for(let declaration of declarations) {
+                element.style = element.style || {}
+                const old = element.style[declaration.property]
+                const nextSpecificity = specificity.reduce((acc, cur) => cur + acc * 65536, 0)
+                if(!old || nextSpecificity > old.specificity) {
+                    element.style[declaration.property] = {
+                        value: declaration.value,
+                        specificity: nextSpecificity
+                    }
+                }
+                console.log('element', element)
+            }
 
-function selectorMathch(selector, element) {
-    // class id type
-    console.log(selector)
-    const regExp = /#[a-z]+|\.[a-z]+|[a-z]+/g
-    let word = null
-    while(word = regExp.exec(selector)) {
-        if(word[0].charAt('#') ) {
-            // id
-        }else if(word[0].charAt('.')) {
-            // class
-        }else{
-            // type
+            // console.log(selectorArr, 'matched', element )
         }
-
     }
 }
+
+   
+
+function selectorMatch(selector, element) {
+    // class id type
+    // console.log(selector)
+    const regExp = /#[a-z]+|\.[a-z]+|[a-z]+/g
+    let word = null
+    let specificity = [0, 0 , 0]
+    while(word = regExp.exec(selector)) {
+        if(word[0].charAt(0) === '#' ) {
+            specificity[0] += 1
+            // console.log(element)
+            const id = element.attributes.id
+            if(word[0].slice(1) !== id) {
+                return false
+            }
+
+            
+            
+        }else if(word[0].charAt(0) === '.') {
+            specificity[1] += 1
+            // class
+            if(!element.attributes.class) {return false}
+            const classList = element.attributes.class.split(' ')
+            if(!classList.includes(word[0].slice(1))) {
+                return false
+            }
+        }else{
+            specificity[2] += 1
+            // type
+            const tagName = element.tagName
+            if(tagName !== word[0]) {
+                return false
+            }
+        }
+        
+
+
+    }
+    return specificity
+}
+
+
+
+
+// function check(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] <= 1) {
+//             return false
+//         }
+//     }
+//     return true
+// }
 
 let current = start
 setEmitCallback ((token) => {
@@ -74,7 +142,7 @@ setEmitCallback ((token) => {
             children: [],
             parent: stack[stack.length -1]
         }
-        
+        // 处理style
         applyStyle(element)
         // console.log(JSON.stringify(stack, null, ' '), 11111)
         stack[stack.length -1]['children'].push(element)
@@ -96,6 +164,7 @@ setEmitCallback ((token) => {
 }) 
 // 栈
 const stack = [{type: 'document', children : []}]
+// html字符串处理
 for(let char of html) {
     // console.log(current.name)
 
--- 03cssParse/index.js
+++ 03cssParse/index.js
@@ -1,3 +1,92 @@
 const css = require('css')
 const obj = css.parse('body{ font-size: 12px;}', {})
 console.log(JSON.stringify(obj, null, ''))
+let a ={
+  "type": "stylesheet",
+  "stylesheet": {
+   "rules": [
+    {
+     "type": "rule",
+     "selectors": [
+      ".num"
+     ],
+     "declarations": [
+      {
+       "type": "declaration",
+       "property": "width",
+       "value": "100px",
+       "position": {
+        "start": {
+         "line": 5,
+         "column": 13
+        },
+        "end": {
+         "line": 5,
+         "column": 25
+        }
+       }
+      },
+      {
+       "type": "declaration",
+       "property": "display",
+       "value": "block",
+       "position": {
+        "start": {
+         "line": 6,
+         "column": 13
+        },
+        "end": {
+         "line": 6,
+         "column": 27
+        }
+       }
+      }
+     ],
+     "position": {
+      "start": {
+       "line": 4,
+       "column": 9
+      },
+      "end": {
+       "line": 7,
+       "column": 10
+      }
+     }
+    },
+    {
+     "type": "rule",
+     "selectors": [
+      ".num"
+     ],
+     "declarations": [
+      {
+       "type": "declaration",
+       "property": "height",
+       "value": "200px",
+       "position": {
+        "start": {
+         "line": 9,
+         "column": 13
+        },
+        "end": {
+         "line": 9,
+         "column": 26
+        }
+       }
+      }
+     ],
+     "position": {
+      "start": {
+       "line": 8,
+       "column": 9
+      },
+      "end": {
+       "line": 10,
+       "column": 10
+      }
+     }
+    }
+   ],
+   "parsingErrors": []
+  }
+ }
